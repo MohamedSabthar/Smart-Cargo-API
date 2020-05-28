ac6da1c1fabdb5f505c0c143e14ae67a2e4d209a
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-05-28 12:19:03 +0530

    vehicle and vehicle-type model re-sturctured

--- models/vehicle-type.js
+++ models/vehicle-type.js
@@ -2,6 +2,9 @@ const mongoose = require("../connection");
 
 const vehicleTypeModel = mongoose.Schema({
   type: { type: Number }, //ex: lorry / three-wheel
+  // store the maximum , load that can be loaded in to the vehicle
+  capacity: { volume: { type: Number }, max_load: { type: String } },
+  fuel_economy: { type: Number }, // Km/l
 });
 
 module.exports = mongoose.model("vehicle_types", vehicleTypeModel);
