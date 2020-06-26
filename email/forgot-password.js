0516456c594b347ec563e5a45a4a4c1bc006ec25
Mohamed Sabthar <sabtharugc0@gmail.com>
2020-06-26 22:38:18 +0530

    password-reset,sendforgot password-mail and forgot-password implemented

---
+++ email/forgot-password.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+const forgotPassword =  require('./password-reset-template');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port : 587,
+  secure : false , // true for 465 port
+  auth: {
+    user: 'third.year.group.project.2020@gmail.com',
+    pass: 'A+everyone'
+  }
+});
+
+const webAppLink = "http://localhost:4200/reset-password";
+
+//this function require user(recipeint) object and a token
+//token used to verify the user authenticy while resetting the password
+module.exports = (recipient,token) => {
+  console.log(recipient.contact.email)
+    const mailOptions = {
+        from: 'SmartCargo',
+        to: recipient.contact.email,
+        subject: 'SmartCargo forgot your password!',
+        html: forgotPassword(`${recipient.name.first} ${recipient.name.last}`,`${webAppLink}/${token}`)
+      };
+
+      transporter.sendMail(mailOptions, (error, info)=> {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
