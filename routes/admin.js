f4547290b1193f75f286236374e8c9889384b7b0
Aksshar <rameshakshar700@gmail.com>
2020-07-12 16:42:59 +0530

    admin middleware uncommented

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const depotModel = require('../models/depot');
 const adminMiddleware = require('../middleware/admin-middleware');
 
 //only admin can execute all the functions implemented here
-//router.use(adminMiddleware);
+router.use(adminMiddleware);
 
 //driver-registration
 router.post('/register-driver', async (req, res) => {
