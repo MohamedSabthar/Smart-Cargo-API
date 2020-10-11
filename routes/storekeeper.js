64e1f23159f251559c3e76e63e6b82632e7fd28e
Faizceo <mohommed.faiz@gmail.com>
2020-10-11 21:55:45 +0530

    changes to assign-driver function

--- routes/storekeeper.js
+++ routes/storekeeper.js
@@ -78,7 +78,7 @@ router.get("/vehicle-types/:id", (req, res) => {
 router.post("/make-cluster", async (req, res) => {
   // get the request(emergancy level) form client
   const emergancyLvl = req.body.emergancyLevels;
-//validating the input
+  //validating the input
   if (emergancyLvl == null || emergancyLvl == [])
     return res.status(400).json({ error: "emergancyLevels can't be empty" });
   console.log(emergancyLvl);
@@ -123,7 +123,7 @@ router.post("/make-cluster", async (req, res) => {
       let schedule = response.data;
       schedule.forEach((doc) => {
         doc.date = new Date(Date.now());
-        doc.storekeeper = req.middleware._id;//accessing the user object from middleware and assing it to schedule object
+        doc.storekeeper = req.middleware._id; //accessing the user object from middleware and assing it to schedule object
         clusteredOrders.push(...doc.orders);
       });
       console.log(clusteredOrders);
@@ -133,7 +133,7 @@ router.post("/make-cluster", async (req, res) => {
 
       await orderModel.updateMany(
         { _id: { $in: clusteredOrders } },
-        { $set: { status: "clustered" } },
+        { $set: { status: "clustered" } }
       );
 
       return res.json({ schedule: result });
@@ -175,7 +175,7 @@ router.put("/assign-driver-to-cluster", (req, res) => {
     .findByIdAndUpdate(
       req.body._id,
       { $set: { driver: req.body.driver } },
-      { new: true },
+      { new: true }
     )
     .exec()
     .then((cluster) => {
@@ -287,7 +287,7 @@ router.post("/generate-route", async (req, res) => {
         {
           $set: { route: response.data },
         },
-        { new: true },
+        { new: true }
       );
 
       //return the route with populated orders
@@ -306,7 +306,7 @@ router.post("/generate-route", async (req, res) => {
       //update the status of routed orders as sheduled
       await orderModel.updateMany(
         { _id: { $in: response.data } },
-        { $set: { status: "sheduled" } },
+        { $set: { status: "sheduled" } }
       );
 
       return res.status(200).json({ route: route });
@@ -371,6 +371,38 @@ router.get("/depot", (req, res) => {
     .exec()
     .then((depot) => {
       return res.status(200).json({ depot: depot });
+    });
+});
+
+router.get("/available-drivers", (req, res) => {
+  userModel
+    .find({ $or: [{ deleted: { $exists: false } }, { deleted: false }] })
+    .where("role")
+    .equals("driver")
+    .where("user_is_available")
+    .equals("true")
+    .exec()
+    .then((drivers) => {
+      return res.status(200).json({ drivers: drivers });
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: err });
+    });
+});
+
+router.put("/assign-driver", (req, res) => {
+  userModel
+    .findByIdAndUpdate(
+      req.body._id,
+      { $set: { user_is_available: false } },
+      { new: true }
+    )
+    .exec()
+    .then((drivers) => {
+      return res.status(200).json({ drivers: drivers });
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: err });
     });
 });
 
